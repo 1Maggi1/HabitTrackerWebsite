e85240f1d8a7a8be7f4f32b27e28bd8716256396
Magnus <magnus.flrian@gmail.com>
2025-06-30 17:15:16 +0200

    Made a Row with all the Habits

--- Habit_Tracker_Website/Controllers/HomeController.cs
+++ Habit_Tracker_Website/Controllers/HomeController.cs
@@ -1,21 +1,24 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using HabitTracker.Models;
+using HabitTracker.Data.Habits;
 
 namespace HabitTracker.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly DatabaseHabitRepository _habitRepository;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, DatabaseHabitRepository habitRepository)
     {
+        _habitRepository = habitRepository;
         _logger = logger;
     }
 
     public IActionResult Index()
     {
-        return View();
+        return View(_habitRepository.GetAllHabits());
     }
 
     public IActionResult Statistics()
