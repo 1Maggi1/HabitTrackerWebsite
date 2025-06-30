e4c93bec0682cad1deedbb1aded29220f1f5c3dc
Magnus <magnus.flrian@gmail.com>
2025-06-30 16:49:58 +0200

    Modified Habit.cs

--- Habit_Tracker_Website/Models/Habit.cs
+++ Habit_Tracker_Website/Models/Habit.cs
@@ -1,18 +1,34 @@
-namespace HabitTracker.Models
-{
-    public class Habit
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public List<DateOnly> completedDates { get; set; }
-        public bool IsActive { get; set; }
+using System.ComponentModel.DataAnnotations;
 
-        public override bool Equals(object? obj)
-        {
-            return obj is Habit habit && habit.Id == Id;
-        }
+namespace HabitTracker.Models;
+
+public class Habit
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required.")]
+    public required string Name { get; set; }
+
+    [Required(ErrorMessage = "Description is required.")]
+    public required string Description { get; set; }
+
+    [Required]
+    public DateTime? CreatedAt { get; set; }
+
+    [Required]
+    public List<DateOnly> CompletedDates { get; set; } = new();
+
+    [Required]
+    public bool? IsActive { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Habit habit && habit.Id == Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
 
     }
-}
